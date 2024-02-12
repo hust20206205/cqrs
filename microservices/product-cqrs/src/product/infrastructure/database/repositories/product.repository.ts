dcf2464eebee898cf7fe91b6a2d17eaf674eb79e
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 21:26:53 +0700

    update product.adapter.ts and product.repository.ts

--- microservices/product-cqrs/src/product/infrastructure/database/repositories/product.repository.ts
+++ microservices/product-cqrs/src/product/infrastructure/database/repositories/product.repository.ts
@@ -2,26 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-// import { Product } from '../../../core/domain/entities/product';
-// import { ProductEntity } from '../entities/product.entity';
-// import { ProductAdapter } from '../mappers/product.adapter';
+import { Product } from 'src/product/core/domain/entities/product';
+import { ProductEntity } from '../entities/product.entity';
+import { ProductAdapter } from '../mappers/product.adapter';
 
-import { CreateProductPort } from 'src/product/core/application/ports/create-product.port';
+import { CreateProductPort } from 'src/product/core/application/commands/create-product/create-product.port';
+import { FindAllProductPort } from 'src/product/core/application/queries/find-all-product/find-all-product.port';
 
 @Injectable()
-export class OrmProductRepository implements CreateProductPort {
+export class OrmProductRepository
+  implements CreateProductPort, FindAllProductPort
+{
   constructor(
     @InjectRepository(ProductEntity)
     private readonly productRepository: Repository<ProductEntity>,
   ) {}
-//   async save(product: Product): Promise<Product> {
-//     const persistenceModel = ProductAdapter.toPersistence(product);
-//     const newEntity = await this.productRepository.save(persistenceModel);
-//     return ProductAdapter.toDomain(newEntity);
-//   }
 
-//   // async findAll(): Promise<productReadModel[]> {
-//   //     return await this.productModel.find();
-//   //   }
-//   // }
-// }
+  async save(product: Product): Promise<Product> {
+    const persistenceModel = ProductAdapter.toPersistence(product);
+    const newEntity = await this.productRepository.save(persistenceModel);
+    return ProductAdapter.toDomain(newEntity);
+  }
+  async findAll(): Promise<Product[]> {
+    const  entities =  await this.productRepository.find();
+
+    return entities.map((item)=>
+    ProductAdapter.toDomain(item))
+  }
+}
