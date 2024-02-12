25a4a4190157afe644c98534529fe62f57d3a6b8
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 20:59:32 +0700

    update product.module.ts

--- microservices/product-cqrs/src/product/product.module.ts
+++ microservices/product-cqrs/src/product/product.module.ts
@@ -3,10 +3,28 @@ import { ProductService } from './core/domain/product.service';
 import { ProductController } from './interface/controllers/product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './infrastructure/entities/product.entity';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ProductInterface } from './interface/product.interface';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
-  controllers: [ProductController],
-  providers: [ProductService],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    // TypeOrmModule.forFeature([...ProductInfrastructure.repositories]),
+    CqrsModule,
+  
+  
+  
+  
+  
+  
+  
+  ],
+  controllers: [...ProductInterface.controllers],
+  providers: [ProductService,
+    // ...ProductInterface.resolvers,
+    // ...ProductInfrastructure.providers,
+    // ...ProductApplications,
+  ],
+  exports: [],
 })
 export class ProductModule {}
