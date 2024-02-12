64c239f3c97c1c716ff54b63800df175d8e51073
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 21:49:54 +0700

    update main.ts

---
+++ microservices/hello/src/domain/product.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductCommand } from '../application/commands/create-product/create-product.command';
+
+@Injectable()
+export class ProductService {
+  constructor() {} // private readonly productRepository: Repository<Product>, // @InjectRepository(Product)
+  async create(createProductCommand: CreateProductCommand) {
+    // import { randomUUID } from 'crypto';
+    // const newProduct = this.productRepository.create(createProductDto);
+    // return await this.productRepository.save(newProduct);
+  }
+
+  async findAll() {
+    // return await this.productRepository.find();
+  }
+
+  //   async findOne(id: string): Promise<Product> {
+  //     return await this.productRepository.findOneBy({ productId: id });
+  //   }
+
+  //   async update(
+  //     id: string,
+  //     updateProductDto: UpdateProductDto,
+  //   ): Promise<Product> {
+  //     await this.productRepository.update(id, updateProductDto);
+  //     return await this.productRepository.findOneBy({ productId: id });
+  //   }
+
+  //   async remove(id: string): Promise<void> {
+  //     await this.productRepository.delete(id);
+  //   }
+}
+
+//   async create(createProductCommand: CreateProductCommand) {
+//     try {
+//       const newProduct = Product.Builder(randomUUID())
+//         .withName(new ProductName(createProductCommand.name))
+//         .withCreatedAt(new Date())
+//         .build();
+//       console.log(newProduct);
+//       return newProduct;
+//       // const newProduct = this.productRepository.create(
+//       //   createProductCommand.name
+//       // );
+
+//       // return await this.productRepository.save(newProduct);
+//       // }
+//     } catch (error) {
+//       return { error: error.message };
+//     }
+//   }
