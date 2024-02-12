b5595943ae9f869ffc20bc4ffde389532c080e70
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 16:29:28 +0700

    update 5 files

--- microservices/product-cqrs/src/app.module.ts
+++ microservices/product-cqrs/src/app.module.ts
@@ -6,3 +6,35 @@ import { Module } from '@nestjs/common';
   providers: [],
 })
 export class AppModule {}
+
+
+
+
+// import { Module } from '@nestjs/common';
+// import { UserModule } from './user/infrastructure/user.module';
+// import { MariaDBTypeOrmClient } from './shared/infrastructure/persistence/typeorm/MariaDBTypeOrmClient';
+// import { APP_FILTER } from '@nestjs/core';
+// import { AllExceptionFilter } from './shared/infrastructure/all-exception.filter';
+// import { ConfigModule } from '@nestjs/config';
+// import * as Joi from '@hapi/joi';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       validationSchema: Joi.object({
+//         DATABASE_NAME: Joi.string().required(),
+//         DATABASE_USERNAME: Joi.string().required(),
+//         DATABASE_PASSWORD: Joi.string().required(),
+//       }),
+//     }),
+//     MariaDBTypeOrmClient.create(),
+//     UserModule,
+//   ],
+//   providers: [
+//     {
+//       provide: APP_FILTER,
+//       useClass: AllExceptionFilter,
+//     },
+//   ],
+// })
+// export class AppModule {}
