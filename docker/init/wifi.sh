6ac579f9373cbb56d22dae9bf44950546d331152
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 17:21:47 +0700

    update wifi.sh

--- docker/init/wifi.sh
+++ docker/init/wifi.sh
@@ -2,18 +2,18 @@
 echo "Tải offline"
 
 echo "Tải image mysql"
-docker pull mysql
-docker save -o mysql_image.tar mysql
+# docker pull mysql
+# docker save -o mysql_image.tar mysql
 docker load -i mysql_image.tar
 echo "mysql Done!"
 
 
 
-echo "Tải image node"
-docker pull node
-docker save -o node_image.tar node
-docker load -i node_image.tar
-echo "node Done!"
+# echo "Tải image node"
+# docker pull node
+# docker save -o node_image.tar node
+# docker load -i node_image.tar
+# echo "node Done!"
 
 
 
