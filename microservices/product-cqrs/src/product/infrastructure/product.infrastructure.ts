8e9c0baac3aa4bb5a59b58c6ddcbd4bbe6635d6f
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 21:29:49 +0700

    update product.infrastructure.ts

--- microservices/product-cqrs/src/product/infrastructure/product.infrastructure.ts
+++ microservices/product-cqrs/src/product/infrastructure/product.infrastructure.ts
@@ -1,17 +1,11 @@
 import { ProductEntity } from './database/entities/product.entity';
 
 export const ProductInfrastructure = {
-  providers: [],
+  providers: [
+    // {
+    //   provide: FindAlarmRepository,
+    //   useClass: OrmFindAlarmRepository,
+    // },
+  ],
   repositories: [ProductEntity],
 };
-// import { ProductEntity } from './orm/entities/product.entity';
-
-// export const ProductInfrastructure = {
-//   providers: [
-//     {
-//       // provide: FindAlarmRepository,
-//       // useClass: OrmFindAlarmRepository,
-//     },
-//   ],
-//   repositories: [ProductEntity],
-// };
