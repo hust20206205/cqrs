077d7529a4ab0213c9adba9dec4fa9670cccf6dd
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 17:32:47 +0700

    update 3 files and delete 1 file

--- microservices/product-cqrs/src/product/product.module.ts
+++ microservices/product-cqrs/src/product/product.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './core/product.service';
 import { ProductController } from './interface/product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+import { Product } from './infrastructure/entities/product.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
