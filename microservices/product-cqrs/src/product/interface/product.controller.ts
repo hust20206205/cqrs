49ee19f8ed0a85d5d58544fa9dc01ede2f28b8bc
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 17:30:30 +0700

    delete 2 files and update 1 file

--- microservices/product-cqrs/src/product/interface/product.controller.ts
+++ microservices/product-cqrs/src/product/interface/product.controller.ts
@@ -9,12 +9,12 @@ import {
   Body,
 } from '@nestjs/common';
 
-import { Product } from './entities/product.entity';
-import { ProductService } from './product.service';
+import { Product } from '../entities/product.entity';
+import { ProductService } from '../core/product.service';
 
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { ResponseProductDto } from './dto/response-product.dto';
+import { CreateProductDto } from '../dto/create-product.dto';
+import { UpdateProductDto } from '../dto/update-product.dto';
+import { ResponseProductDto } from '../dto/response-product.dto';
 
 import {
   ApiBody,
--- microservices/product-cqrs/src/product/product.module.ts
+++ microservices/product-cqrs/src/product/product.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
-import { ProductController } from './product.controller';
+import { ProductService } from './core/product.service';
+import { ProductController } from './interface/product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 
