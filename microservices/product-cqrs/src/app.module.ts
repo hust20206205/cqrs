1db2a1b885456be516bfc5d7f36e5f9a43840731
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 16:37:12 +0700

    update app.module.ts and product.module.ts

--- microservices/product-cqrs/src/app.module.ts
+++ microservices/product-cqrs/src/app.module.ts
@@ -1,52 +1,27 @@
 import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// import { Module } from '@nestjs/common';
-// import { UserModule } from './user/infrastructure/user.module';
+import { ProductModule } from './product/product.module';
 // import { MariaDBTypeOrmClient } from './shared/infrastructure/persistence/typeorm/MariaDBTypeOrmClient';
 // import { APP_FILTER } from '@nestjs/core';
 // import { AllExceptionFilter } from './shared/infrastructure/all-exception.filter';
 // import { ConfigModule } from '@nestjs/config';
 // import * as Joi from '@hapi/joi';
+//
 
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       validationSchema: Joi.object({
-//         DATABASE_NAME: Joi.string().required(),
-//         DATABASE_USERNAME: Joi.string().required(),
-//         DATABASE_PASSWORD: Joi.string().required(),
-//       }),
-//     }),
-//     MariaDBTypeOrmClient.create(),
-//     UserModule,
-//   ],
-//   providers: [
-//     {
-//       provide: APP_FILTER,
-//       useClass: AllExceptionFilter,
-//     },
-//   ],
-// })
-// export class AppModule {}
+@Module({
+  imports: [
+    //     ConfigModule.forRoot({
+    //       validationSchema: Joi.object({
+    //         DATABASE_NAME: Joi.string().required(),
+    //         DATABASE_USERNAME: Joi.string().required(),
+    //         DATABASE_PASSWORD: Joi.string().required(),
+    //       }),
+    //     }),
+    ProductModule,
+  ],
+  //   providers: [
+  //     {
+  //       provide: APP_FILTER,
+  //       useClass: AllExceptionFilter,
+  //     },
+})
+export class AppModule {}
